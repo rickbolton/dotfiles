769a599d8b736da4b6460b7aa46fbb8ef6171bce
Rick Bolton <rick.bolton@nandos-ltmb-164.lan>
2022-10-05 11:11:29 +0100

    updates

--- link.sh
+++ link.sh
@@ -1,10 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
 
-echo "Linking neovim"
-ln -s $(pwd)/.config/nvim ~/.config/nvim
+########## Variables
 
-# echo "Linking tmux"
-# ln -s $(pwd)/tmux.conf ~/.tmux.conf
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="zshrc config/nvim"    # list of files/folders to symlink in homedir
 
-echo "Linking zshrc"
-ln -s $(pwd)/.zshrc ~/.zshrc
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/.$file ~/.$file
+done
